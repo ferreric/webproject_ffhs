86aa2725156927fad561c71e4f51338f96f0dcf4
Eric Ferrari-Herrmann <eric.ferrari-herrmann@students.ffhs.ch>
2023-01-25 16:46:06 +0100

    FlexStyler Schritt 1u2 getan!

--- semesterarbeit/exercises/flexStyler2/script.js
+++ semesterarbeit/exercises/flexStyler2/script.js
@@ -1,3 +1,5 @@
+//Schritt 1 - Options
+
 const flexDirectionOptions = new Array ('row', 'row-reverse', 'column', 'column-reverse')
 const justifyContentOptions = new Array ('flex-start','flex-end','center','space-between','space-around','space-evenly')
 const alignItemsOptions = new Array ('flex-start','flex-end','center','stretch','baseline')
@@ -6,12 +8,51 @@ const justifyMenu = document.getElementById('select-justify-content')
 const alignMenu = document.getElementById('select-align-items')
 
 console.log(flexDirectionOptions)
-const fillOptions = (menu, option) => {
+const fillFlexOptions = (option) => {
     const newOption = document.createElement('option')
     newOption.setAttribute('value', option)
     const text = document.createTextNode(option)
     newOption.appendChild(text)
-    menu.appendChild(newOption)
+    flexMenu.appendChild(newOption)
 }
 
-fillOptions(flexMenu, flexDirectionOptions[0])
+flexDirectionOptions.forEach(fillFlexOptions)
+const fillJustifyContentOptions = (option) => {
+    const newOption = document.createElement('option')
+    newOption.setAttribute('value', option)
+    const text = document.createTextNode(option)
+    newOption.appendChild(text)
+    justifyMenu.appendChild(newOption)
+}
+
+justifyContentOptions.forEach(fillJustifyContentOptions)
+const fillAlignItemsOptions = (option) => {
+    const newOption = document.createElement('option')
+    newOption.setAttribute('value', option)
+    const text = document.createTextNode(option)
+    newOption.appendChild(text)
+    alignMenu.appendChild(newOption)
+}
+
+alignItemsOptions.forEach(fillAlignItemsOptions)
+
+//Schritt 2 - Change & Click Events
+
+const flexContainer = document.getElementById('flex-container')
+flexMenu.addEventListener('change', (e) =>{
+})
+
+const formWrap = document.querySelector('.form-wrap')
+
+formWrap.addEventListener('change', (e) => { // one listener. event propagates.
+    console.log('event thrown ' + e)
+    flexContainer.style.flexDirection = flexDirectionOptions[flexMenu.options.selectedIndex]
+    flexContainer.style.justifyContent = justifyContentOptions[justifyMenu.options.selectedIndex]
+    flexContainer.style.alignItems = alignItemsOptions[alignMenu.options.selectedIndex]
+})
+
+const checkBoxWrap = document.getElementById('chk-wrap')
+checkBoxWrap.addEventListener('click', (e) => {
+    console.log('checkbox ticked: ' + checkBoxWrap.checked)
+    checkBoxWrap.checked ? (flexContainer.style.flexWrap = 'wrap') : (flexContainer.style.removeProperty('flex-wrap'))
+})
