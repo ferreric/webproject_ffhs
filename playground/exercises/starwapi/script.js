febf70f7f8375236a927a8a2edc04e64bb01f502
Eric Ferrari-Herrmann <eric.ferrari-herrmann@students.ffhs.ch>
2023-01-29 00:29:23 +0100

    ships load!

--- playground/exercises/starwapi/script.js
+++ playground/exercises/starwapi/script.js
@@ -3,32 +3,12 @@
 // Implement the load more button
 // Implement the "details" button to show the ship info in the div below
 
-fetch('https://swapi.dev/api/starships/9/')
+fetch('https://swapi.dev/api/starships/')
     .then(response => response.json())
     .then(data => console.log(data))
 
-let ships = {}
-for (let i = 2; i<=3; i++){ // i == 32 sollte funktionieren; mit lücken
-    fetch(`https://swapi.dev/api/starships/${i}/`)
-        .then(response => response.json())
-        .then(data => ships+=data)
-}
-console.log(ships)
-
-let htmlSegment = `
-    <article class="starship">
-      <h2>${ship.name}</h2>
-        <div>
-          <span>Model: </span>
-          ${ship.model}
-        </div>
-        <button>Details!</button>
-    </article>`
-
-let shipSection = document.querySelector('.starship-list')
-
-async function getShips(i) {
-    let url = `https://swapi.dev/api/starships/${i}/`;
+async function getShips() {
+    let url = `https://swapi.dev/api/starships/`;
     try {
         let res = await fetch(url);
         return await res.json();
@@ -36,3 +16,27 @@ async function getShips(i) {
         console.log(error);
     }
 }
+
+async function renderShips() {
+    let ships = await getShips()
+    console.log(ships)
+    let html = '';
+    ships.results.forEach(ship => {
+        let htmlSegment = `
+            <article class="starship">
+              <h2>${ship.name}</h2>
+                <div>
+                  <span>Model: </span>
+                  ${ship.model}
+                </div>
+                <button>Details!</button>
+            </article>`
+
+        html += htmlSegment;
+    });
+
+    let shipSection = document.querySelector('.starship-list')
+    shipSection.innerHTML = html;
+}
+
+renderShips()
