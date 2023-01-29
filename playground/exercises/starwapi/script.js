d12f3406de66b66027a2272903efdd5a6725e7a6
Eric Ferrari-Herrmann <eric.ferrari-herrmann@students.ffhs.ch>
2023-01-30 00:14:28 +0100

    js fertig; style nötig!

--- playground/exercises/starwapi/script.js
+++ playground/exercises/starwapi/script.js
@@ -1,14 +1,7 @@
 // Grab the ships data from swapi.dev
-// Replace the starship sections with actual data
-// Implement the load more button
-// Implement the "details" button to show the ship info in the div below
 
-fetch('https://swapi.dev/api/starships/')
-    .then(response => response.json())
-    .then(data => console.log(data))
-
-async function getShips() {
-    let url = `https://swapi.dev/api/starships/`;
+async function getShips(page = 1) {
+    let url = `https://swapi.dev/api/starships/?page=${page}`;
     try {
         let res = await fetch(url);
         return await res.json();
@@ -17,9 +10,9 @@ async function getShips() {
     }
 }
 
-async function renderShips() {
-    let ships = await getShips()
-    console.log(ships)
+// Replace the starship sections with actual data
+async function renderShips(page = 1) {
+    let ships = await getShips(page)
     let html = '';
     ships.results.forEach(ship => {
         let htmlSegment = `
@@ -29,14 +22,52 @@ async function renderShips() {
                   <span>Model: </span>
                   ${ship.model}
                 </div>
-                <button>Details!</button>
+                <button onclick="renderDetail('${ship.url}')">Details!</button>
             </article>`
 
         html += htmlSegment;
     });
-
     let shipSection = document.querySelector('.starship-list')
-    shipSection.innerHTML = html;
+    shipSection.innerHTML += html;
 }
 
 renderShips()
+
+// Implement the load more button
+const moreButton = document.getElementById('load-more')
+let pageCount = 1
+moreButton.addEventListener('click', () => {
+    if (pageCount < 4) {
+        renderShips(++pageCount)
+    }
+    else {
+        moreButton.setAttribute('disabled', 'true')
+        moreButton.innerText = "End reached"
+    }
+})
+
+// Implement the "details" button to show the ship info in the div below
+async function getDetails(ship = 'https://swapi.dev/api/starships/74') {
+    let url = `${ship}`;
+    try {
+        let res = await fetch(url);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function renderDetail(ship = 'https://swapi.dev/api/starships/74') {
+    let details = await getDetails(ship)
+    console.log(details)
+    let html = `
+              <p>Name: ${details.name}</p>
+              <p>Model: ${details.model}</p>
+              <p>Cargo Capacity: ${details.cargo_capacity}</p>
+              <p>Consumables: ${details.consumables}</p>
+              <p>Cost in credits: ${details.cost_in_credits}</p>
+              `
+    let detailSection = document.querySelector('.starship-detail')
+    detailSection.innerHTML = html;
+}
+
+//renderDetail('https://swapi.dev/api/starships/2')
